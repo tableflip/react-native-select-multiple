70e46072f52934a50b48398bf6f5bc2b1116f167
NaveedEatMe <dev3@eatmeglobal.com>
2019-05-28 13:44:53 +0530

    removed formatting

--- src/SelectMultiple.js
+++ src/SelectMultiple.js
@@ -1,29 +1,23 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import {
-  View,
-  FlatList,
-  Text,
-  TouchableWithoutFeedback,
-  Image
-} from "react-native";
-import styles from "./SelectMultiple.styles";
-import checkbox from "../images/icon-checkbox.png";
-import checkboxChecked from "../images/icon-checkbox-checked.png";
-import { mergeStyles } from "./style";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { View, FlatList, Text, TouchableWithoutFeedback, Image } from 'react-native'
+import styles from './SelectMultiple.styles'
+import checkbox from '../images/icon-checkbox.png'
+import checkboxChecked from '../images/icon-checkbox-checked.png'
+import { mergeStyles } from './style'
 
 const itemType = PropTypes.oneOfType([
   PropTypes.string,
   PropTypes.shape({ label: PropTypes.any, value: PropTypes.any })
-]);
+])
 
 const styleType = PropTypes.oneOfType([
   PropTypes.object,
   PropTypes.number,
   PropTypes.array
-]);
+])
 
-const sourceType = PropTypes.oneOfType([PropTypes.object, PropTypes.number]);
+const sourceType = PropTypes.oneOfType([PropTypes.object, PropTypes.number])
 
 // A customiseable FlatList that allows you to select multiple rows
 export default class SelectMultiple extends Component {
@@ -46,7 +40,7 @@ export default class SelectMultiple extends Component {
     selectedRowStyle: styleType,
     selectedCheckboxStyle: styleType,
     selectedLabelStyle: styleType
-  };
+  }
 
   static defaultProps = {
     selectedItems: [],
@@ -58,108 +52,105 @@ export default class SelectMultiple extends Component {
     checkboxSource: checkbox,
     selectedCheckboxSource: checkboxChecked,
     renderLabel: null
-  };
+  }
 
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
-    this.state = { dataSource: [] };
+    this.state = { dataSource: [] }
   }
   componentDidMount() {
     const rows = this.getRowData(this.props);
     this.setState({ dataSource: rows });
   }
-  componentWillReceiveProps(nextProps) {
-    const rows = this.getRowData(nextProps);
-    this.setState({ dataSource: rows });
+  componentWillReceiveProps (nextProps) {
+    const rows = this.getRowData(nextProps)
+    this.setState({ dataSource: rows })
   }
 
-  getRowData({ items, selectedItems }) {
-    items = items.map(this.toLabelValueObject);
-    selectedItems = (selectedItems || []).map(this.toLabelValueObject);
+  getRowData ({ items, selectedItems }) {
+    items = items.map(this.toLabelValueObject)
+    selectedItems = (selectedItems || []).map(this.toLabelValueObject)
 
-    items.forEach(item => {
-      item.selected = selectedItems.some(i => i.value === item.value);
-    });
+    items.forEach((item) => {
+      item.selected = selectedItems.some((i) => i.value === item.value)
+    })
 
-    return items;
+    return items
   }
 
-  onRowPress(row) {
-    const { label, value } = row;
-    let { selectedItems } = this.props;
+  onRowPress (row) {
+    const { label, value } = row
+    let { selectedItems } = this.props
 
-    selectedItems = (selectedItems || []).map(this.toLabelValueObject);
+    selectedItems = (selectedItems || []).map(this.toLabelValueObject)
 
-    const index = selectedItems.findIndex(
-      selectedItem => selectedItem.value === value
-    );
+    const index = selectedItems.findIndex((selectedItem) => selectedItem.value === value)
 
     if (index > -1) {
-      selectedItems = selectedItems.filter(
-        selectedItem => selectedItem.value !== value
-      );
+      selectedItems = selectedItems.filter((selectedItem) => selectedItem.value !== value)
     } else {
-      selectedItems = selectedItems.concat({ label, value });
+      selectedItems = selectedItems.concat({ label, value })
     }
 
-    this.props.onSelectionsChange(selectedItems, { label, value });
+    this.props.onSelectionsChange(selectedItems, { label, value })
   }
 
-  toLabelValueObject(obj) {
-    if (Object.prototype.toString.call(obj) === "[object String]") {
-      return { label: obj, value: obj };
+  toLabelValueObject (obj) {
+    if (Object.prototype.toString.call(obj) === '[object String]') {
+      return { label: obj, value: obj }
     } else {
-      return { label: obj.label, value: obj.value };
+      return { label: obj.label, value: obj.value }
     }
   }
 
-  keyExtractor = (item, index) => index.toString();
+  keyExtractor = (item, index) => index.toString()
 
-  render() {
-    const { dataSource } = this.state;
-    const { style, flatListProps, keyExtractor } = this.props;
-    return (
-      <FlatList
-        style={style}
-        keyExtractor={keyExtractor || this.keyExtractor}
-        data={dataSource}
-        renderItem={this.renderItemRow}
-        {...flatListProps}
-      />
-    );
+  render () {
+    const { dataSource } = this.state
+    const { style, flatListProps, keyExtractor } = this.props
+    return <FlatList
+      style={style}
+      keyExtractor={keyExtractor || this.keyExtractor}
+      data={dataSource}
+      renderItem={this.renderItemRow}
+      {...flatListProps}
+    />
   }
 
   renderLabel = (label, style, selected) => {
     if (this.props.renderLabel) {
-      return this.props.renderLabel(label, style, selected);
+      return this.props.renderLabel(label, style, selected)
     }
-    return <Text style={style}>{label}</Text>;
-  };
+    return (
+      <Text style={style}>{label}</Text>
+    )
+  }
 
-  renderItemRow = row => {
-    let { checkboxSource, rowStyle, labelStyle, checkboxStyle } = this.props;
+  renderItemRow = (row) => {
+    let {
+      checkboxSource,
+      rowStyle,
+      labelStyle,
+      checkboxStyle
+    } = this.props
 
     const {
       selectedCheckboxSource,
       selectedRowStyle,
       selectedCheckboxStyle,
       selectedLabelStyle
-    } = this.props;
+    } = this.props
 
     if (row.item.selected) {
-      checkboxSource = selectedCheckboxSource;
-      rowStyle = mergeStyles(styles.row, rowStyle, selectedRowStyle);
-      checkboxStyle = mergeStyles(
-        styles.checkbox,
-        checkboxStyle,
-        selectedCheckboxStyle
-      );
-      labelStyle = mergeStyles(styles.label, labelStyle, selectedLabelStyle);
+      checkboxSource = selectedCheckboxSource
+      rowStyle = mergeStyles(styles.row, rowStyle, selectedRowStyle)
+      checkboxStyle = mergeStyles(styles.checkbox, checkboxStyle, selectedCheckboxStyle)
+      labelStyle = mergeStyles(styles.label, labelStyle, selectedLabelStyle)
     } else {
-      rowStyle = mergeStyles(styles.row, rowStyle);
-      checkboxStyle = mergeStyles(styles.checkbox, checkboxStyle);
-      labelStyle = mergeStyles(styles.label, labelStyle);
+      rowStyle = mergeStyles(styles.row, rowStyle)
+      checkboxStyle = mergeStyles(styles.checkbox, checkboxStyle)
+      labelStyle = mergeStyles(styles.label, labelStyle)
     }
 
     return (
@@ -169,6 +160,6 @@ export default class SelectMultiple extends Component {
           {this.renderLabel(row.item.label, labelStyle, row.item.selected)}
         </View>
       </TouchableWithoutFeedback>
-    );
-  };
+    )
+  }
 }
